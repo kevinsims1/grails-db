5cfb029dc1395e9dfe8f541c489f8089c99e514a
kevinsims1 <kevindsims1@gmail.com>
2020-07-02 15:27:59 -0700

    crud

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,5 @@
 const Admin = require('../models/admin')
+const Item = require('../models/item')
 const bcrypt = require('bcrypt')
 
 module.exports = {
@@ -11,9 +12,7 @@ module.exports = {
 
       bcrypt.hash(password, 12, async function(err, hash) {
         try{
-          console.log('hellooo')
           const user =  await Admin.create({ ...req.body, password: hash })
-          console.log(user)
           res.status(200).json({ user })
         }catch(err){
           res.status(404).json({ err })
@@ -28,7 +27,6 @@ module.exports = {
       const {email,password} = req.body
       if(email && password){
         const valid = Admin.findOne({email})
-        console.log(valid)
         res.status(200).json({ message: 'success' })
       }
       throw new Error('check email and password')
@@ -36,4 +34,44 @@ module.exports = {
       res.status(404).json({ err })
     }
   },
+  createItem: async(req, res) => {
+    try{
+      const newItem = await Item.create({...req.body})
+      res.status(200).json({newItem})
+    }catch(err){
+      res.status(500).json({err})
+    }
+  },
+  updateItem: async(req, res) => {
+    try{
+      const updatedItem = await Item.findByIdAndUpdate({_id: req.query.id}, req.body, {new:true}).lean().exec()
+      res.status(200).json({ updatedItem })
+    }catch(err){
+      res.status(500).json({err})
+    }
+  },
+  getItems: async(req, res) => {
+    try{
+      const items = await Item.find( { user: { $in : [req.body.id] } } ).lean().exec()
+      res.status(200).json({items})
+    }catch(err){
+      res.status(500).json({err})
+    }
+  },
+  getItem: async(req, res) => {
+    try{
+      const item = await Item.findById( { _id: req.body.id } ).lean().exec()
+      res.status(200).json({item})
+    }catch(err){
+      res.status(500).json({err})
+    }
+  },
+  deleteItem: async(req, res) => {
+    try{
+      const item = await Item.findByIdAndDelete({_id: req.body.id}).lean().exec()
+      res.status(200).json({item})
+    }catch(err){
+      res.status(500).json({err})
+    }
+  }
 }
--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
-
+const controller = require('../controller')
 router.get('/check', (req,res) => res.send('hello world'))
+router.post('/create', controller.createItem)
+router.post('/update', controller.updateItem)
+router.post('/delete', controller.deleteItem)
+router.get('/all', controller.getItems)
+router.get('/solo', controller.getItem)
 
 
 module.exports = router
